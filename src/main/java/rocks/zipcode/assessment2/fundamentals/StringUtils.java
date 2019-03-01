ae17157511f7e6bec7e2200923d83664ae75e8b6
miscbits <willow6786@gmail.com>
2019-03-01 14:55:02 -0500

    removed methods being removed from exam

--- src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
@@ -32,43 +32,6 @@ public class StringUtils {
     }
 
     /**
-     * @param baseString - String to be evaluated
-     * @param stringToRemove - String to be removed from `baseString`
-     * @return `baseString` with the last occurrence of `stringToRemove` removed
-     */
-    public static String replaceLastSubString(String baseString, String stringToRemove) {
-        return null;
-    }
-
-    /**
-     * @param baseString - String to be evaluated
-     * @param stringToRemove - String to be removed from `baseString`
-     * @return `baseString` with the nth occurrence of `stringToRemove` removed
-     */
-    public static String replaceNthSubstring(String baseString, String stringToRemove, int nthOccurrence) {
-        return null;
-    }
-
-    /**
-     * @param baseString - String to be evaluated
-     * @param subString - String to be identified in `baseString`
-     * @param nthOccurrence - nth-occurrence of `subString` in `baseString`
-     * @return the starting index of the nth-subString
-     */
-    public static Integer getIndexOfNthSubstring(String baseString, String subString, Integer nthOccurrence) {
-        return null;
-    }
-
-    /**
-     * @param baseString - String to be evaluated
-     * @param subString - String to be identified in `baseString`
-     * @return number of occurrences of `subString` in `baseString`
-     */
-    public static Integer getNumberOfOccurrences(String baseString, String subString) {
-        return null;
-    }
-
-    /**
      * @param string - string to be evaluated
      * @return - true if string only contains alpha characters
      */
