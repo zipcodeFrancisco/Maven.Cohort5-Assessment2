9a129b37955f74e64067abc8bde828b9a92f7442
Leon Hunter <leonhunter@Leons-MacBook-Pro.local>
2019-03-03 07:34:33 -0500

    more comments

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -6,14 +6,17 @@ import java.util.List;
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
-    public Inventory() {
-
-    }
-
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
+
+    }
+
+    /**
+     * nullary constructor initializes a new list
+     */
+    public Inventory() {
 
     }
 
