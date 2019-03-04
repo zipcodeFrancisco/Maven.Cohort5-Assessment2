923bf242825102a2485b18f900bf34114806e65f
zipcodeFrancisco <zipcodefrancisco@gmail.com>
2019-03-04 13:28:45 -0500

    133 - 159

--- src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
@@ -5,7 +5,7 @@ package rocks.zipcode.assessment2.objectorientation;
  */
 public class Address {
 
-    
+
 
     public Address() {
     }
@@ -17,42 +17,70 @@ public class Address {
      * @param state - state of city
      * @param zipcode - zipcode of region
      */
+
+     private String addressLine1 = "";
+     private String addressLine2 = "";
+     private String city = "";
+     private String state = "";
+     private String zipcode = "";
+
     public Address(String addressLine1, String addressLine2, String city, String state, String zipcode) {
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
     }
 
     public String getAddressLine1() {
-        return null;
+        return this.addressLine1;
     }
 
     public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
     }
 
     public String getAddressLine2() {
-        return null;
+        return this.addressLine2;
     }
 
     public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
     }
 
     public String getCity() {
-        return null;
+        return this.city;
     }
 
     public void setCity(String city) {
+        this.city = city;
     }
 
     public String getState() {
-        return null;
+        return this.state;
     }
 
     public void setState(String state) {
+        this.state = state;
     }
 
     public String getZipcode() {
-        return null;
+        return this.zipcode;
     }
 
     public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "addressLine1='" + addressLine1 + '\'' +
+                ", addressLine2='" + addressLine2 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                '}';
     }
 
     @Override
